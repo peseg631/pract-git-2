c34b3ba7d96f9860e395ac1550ecb220a096e73a
peseg631 <petrusevas119@gmail.com>
2025-06-12 02:47:10 +0700

    Добавление 4 функции

--- app.py
+++ app.py
@@ -19,5 +19,14 @@ def main():
     else:
         print("Неверный ввод")
 
+def function4():
+    print("Это функция 4 - Деление")
+    a = float(input("Введите первое число: "))
+    b = float(input("Введите второе число: "))
+    if b != 0:
+        print(f"Результат: {a / b}")
+    else:
+        print("Ошибка: деление на ноль!")
+
 if __name__ == "__main__":
     main()
